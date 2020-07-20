cd7fb77c9d76d7628c5acc60d09af7a3c3d28cb2
Daniel Ilett <danielilett@gmail.com>
2020-07-20 12:19:23 +0100

    Added vertex glitching and noise grain.

---
+++ Assets/Scripts/Glitch.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glitch : MonoBehaviour
+{
+    private Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+
+        StartCoroutine(GlitchRoutine());
+    }
+
+    private IEnumerator GlitchRoutine()
+    {
+        while(true)
+        {
+            material.SetFloat("_GlitchStrength", 0.0f);
+            material.SetFloat("_ScanlineOffset", 0.0f);
+            yield return new WaitForSeconds(0.25f);
+
+            material.SetFloat("_GlitchStrength", 0.15f);
+            material.SetFloat("_ScanlineOffset", 0.5f);
+            yield return new WaitForSeconds(0.25f);
+
+            material.SetFloat("_GlitchStrength", 0.0f);
+            material.SetFloat("_ScanlineOffset", 0.0f);
+            yield return new WaitForSeconds(0.5f);
+
+            material.SetFloat("_GlitchStrength", 0.1f);
+            material.SetFloat("_ScanlineOffset", 0.5f);
+            yield return new WaitForSeconds(0.1f);
+
+            material.SetFloat("_GlitchStrength", 0.0f);
+            material.SetFloat("_ScanlineOffset", 0.0f);
+            yield return new WaitForSeconds(0.1f);
+
+            material.SetFloat("_GlitchStrength", 0.05f);
+            material.SetFloat("_ScanlineOffset", 0.5f);
+            yield return new WaitForSeconds(0.1f);
+
+            material.SetFloat("_GlitchStrength", 0.0f);
+            material.SetFloat("_ScanlineOffset", 0.0f);
+            yield return new WaitForSeconds(0.4f);
+
+            material.SetFloat("_GlitchStrength", 0.1f);
+            material.SetFloat("_ScanlineOffset", 0.5f);
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+}
